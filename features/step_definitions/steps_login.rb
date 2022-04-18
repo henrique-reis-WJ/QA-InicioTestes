ea41c695977acc76b2f7ddf03c3fd5e64c1b06a9
Henrique <henrique.reis@webjump.com.br>
2022-04-18 12:49:04 -0300

    Orientando Objetos

--- features/step_definitions/steps_login.rb
+++ features/step_definitions/steps_login.rb
@@ -1,40 +1,31 @@
+login = Login.new
+carrinho = Carrinho.new
+
 Dado("que eu acesso a pagina inicial") do
-    visit 'https://magento.nublue.co.uk/'
+    login.AcessarPagina
   end
 
   Dado("que quero fazer Login") do
-    click_link 'Sign In'
+    login.ClicarLogin
   end
 
   Quando("preencho meus dados {string} e {string}") do |email, senha|
-    @email = email
-    find('#email').set email
-    find('#pass').set senha
-    click_button 'Sign In'
-    sleep 5  
+    login.PreencherDados(email, senha)
+    sleep 5
   end
 
-   Então("devo ler a seguinte mensagem {string}") do |mensagem|
-    expect(page).to have_content mensagem
+   Então("devo ler a seguinte mensagem {string}") do |mensagem_login|
+    login.Autenticado (mensagem_login)
   end
 
   Dado("que escolhi o produto") do
-    find_by_id('ui-id-4').hover
-    sleep 1
-    find('#ui-id-16').hover
-    sleep 1
-    find('#ui-id-20').click
-    sleep 2
-    click_link('Helios EverCool™ Tee')
+    carrinho.EscolherProduto
   end
   
   Quando("adiciono ao carrinho") do
-    find('#option-label-size-144-item-168').click
-    find('#option-label-color-93-item-49').click
-    click_button 'Add to Cart'
-    sleep 5
+    carrinho.CorTamanho
   end
   
-  Então("devo ler a mensagem {string}") do |mensagem_compra|
-    expect(page).to have_content mensagem_compra
+  Então("devo ler a mensagem You added Helios EverCool™ Tee") do
+    carrinho.CarrinhoOk
   end
