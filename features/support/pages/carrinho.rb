9ebd4fc596a453c0856fd1d4e3baa2006f21f0dc
Henrique <henrique.reis@webjump.com.br>
2022-04-18 12:55:39 -0300

    Classe para orientação de objetos Adição no Carrinho

---
+++ features/support/pages/carrinho.rb
@@ -0,0 +1,25 @@
+class Carrinho 
+include Capybara::DSL
+
+def EscolherProduto
+    find_by_id('ui-id-4').hover
+    sleep 1
+    find('#ui-id-16').hover
+    sleep 1
+    find('#ui-id-20').click
+    sleep 3
+    click_link('Helios EverCool™ Tee')
+end
+
+def CorTamanho
+    find('#option-label-size-144-item-168').click
+    find('#option-label-color-93-item-49').click
+    click_button 'Add to Cart'
+    sleep 5
+end
+
+def CarrinhoOk 
+    assert_text('You added')
+end
+
+end
