fad6390e4c09b8f7b8b9d9a9ac072965c0d30a5d
Henrique <henrique.reis@webjump.com.br>
2022-04-18 12:56:00 -0300

    Login orientado a objetos (Classe)

---
+++ features/support/pages/login.rb
@@ -0,0 +1,23 @@
+class Login
+include Capybara::DSL
+
+def AcessarPagina
+    visit 'https://magento.nublue.co.uk/'   
+end
+
+def ClicarLogin
+    click_link 'Sign In'
+end
+
+def PreencherDados (p_email, p_senha)
+    find('#email').set p_email
+    find('#pass').set p_senha
+    click_button 'Sign In'
+    sleep 5
+end
+
+def Autenticado (p_mensagem)
+    assert_text('Veronica Costello')
+end
+
+end
